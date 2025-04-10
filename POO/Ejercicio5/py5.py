e101342389813b4a5d25c54f1c3c3d0799f4ecdc
Mayesalas <117782633+Mayesalas@users.noreply.github.com>
2025-04-10 13:00:10 -0400

    Create py5.py

---
+++ POO/Ejercicio5/py5.py
@@ -0,0 +1,20 @@
+class Estudiante:
+    def __init__(self, nombre, nota_1, nota_2):
+        self.nombre = nombre
+        self.nota_1 = nota_1
+        self.nota_2 = nota_2
+
+    def calcular_promedio(self):
+        return (self.nota_1 + self.nota_2) / 2
+
+    def verificar_aprobado(self):
+        return self.calcular_promedio() >= 6
+
+estudiante1 = Estudiante("Dami", 8.5, 7.0)
+estudiante2 = Estudiante("Ara", 5.0, 6.5)
+estudiante3 = Estudiante("Dorian", 9.0, 8.0)
+
+for estudiante in [estudiante1, estudiante2, estudiante3]:
+    promedio = estudiante.calcular_promedio()
+    aprobado = "Aprobado" if estudiante.verificar_aprobado() else "No Aprobado"
+    print(f"Estudiante: {estudiante.nombre}, Promedio: {promedio:.2f}, Estado: {aprobado}")
